b3074e5a4614e3cb74b24549d1f90dfcd7137d29
fearthebest <mpassone94@gmail.com>
2026-05-18 06:52:38 +0200

    chore: remove remaining helper scripts

--- final-cleanup.cmd
+++ final-cleanup.cmd
@@ -1,5 +1,5 @@
 @echo off
 cd /d "C:\Users\mpass\Documents\GitHub\IKAPPA~2"
 git add -A
-git commit -m "chore: remove local push helper scripts"
+git commit -m "chore: remove remaining helper scripts"
 git push origin main
